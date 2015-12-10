59cca311e71e7980d3959a89f388e7505fddf2a8
bgulbis <brian.gulbis@gmail.com>
2015-12-10 11:12:27 -0600

    make groups of 500

--- screen.R
+++ screen.R
@@ -8,10 +8,11 @@ source("library.R")
 pts.screen <- list.files("Screen", pattern="^Enoxaparin", full.names=TRUE) %>%
     lapply(read.csv, colClasses="character") %>%
     bind_rows %>%
-    transmute(pie.id = PowerInsight.Encounter.Id)
+    transmute(pie.id = PowerInsight.Encounter.Id) %>%
+    distinct
 
 # split the patients up into groups of 1000
-edw.pie <- split(pts.screen$pie.id, ceiling(seq_along(pts.screen$pie.id)/1000))
+edw.pie <- split(pts.screen$pie.id, ceiling(seq_along(pts.screen$pie.id)/500))
 # combine the id's in each group into a string, separated by semi-colon
 edw.pie <- lapply(edw.pie, str_c, collapse=";")
 
