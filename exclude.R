e54e63191c3eb07718eadde436260f2c47abd2bb
bgulbis <brian.gulbis@gmail.com>
2015-12-30 14:14:45 -0600

    exclusion updates

--- exclude.R
+++ exclude.R
@@ -46,3 +46,50 @@ raw.excl.meds <- list.files("Exclusion", pattern="^medications", full.names=TRUE
               route = factor(Route.of.Administration...Short, exclude = "")) 
 
 # evaluate for inclusion criteria: at least 3 doses of enoxaparin over 72 hours
+
+# find those with >= 3 doses and >= 72 hours of treatment
+tmp.count.doses <- raw.excl.meds %>%
+    filter(event == "enoxaparin",
+           dose > 40) %>%
+    group_by(pie.id) %>%
+    arrange(event.datetime) %>%
+    summarize(count = n(),
+              first = first(event.datetime),
+              last = last(event.datetime)) %>%
+    filter(count >= 3,
+           difftime(last, first, units = "hours") >= 72)
+
+enox_course <- function(diff) {
+#     sapply(diff, function(x)
+#         if (is.na(x)) {
+#             course <- 1
+#             course
+#         } else if (x <= 28) {
+#             course
+#         } else {
+#             course <- course + 1
+#             course
+#         }
+#     )
+}
+
+# find patients who had gaps in enoxaparin therapy (> 30 hours between doses)
+tmp.courses <- raw.excl.meds %>%
+    filter(pie.id %in% tmp.count.doses$pie.id,
+           event == "enoxaparin",
+           dose > 40) %>%
+    group_by(pie.id) %>%
+    arrange(event.datetime) %>%
+    mutate(hours.prev.dose = difftime(event.datetime, lag(event.datetime), units = "hours"),
+           course.start = ifelse(is.na(hours.prev.dose) | hours.prev.dose > 36, TRUE, FALSE)) 
+    
+tmp.courses.count <- summarize(tmp.courses, start = sum(course.start)) %>%
+    ungroup %>%
+    group_by(start) %>%
+    summarize(count = n())
+
+# exclusion: pregnancy, weight < 45 or > 150, >1 CrCl <30 during enoxaparin,
+# coadmin of other anticoags except warfarin, enoxaparin dose change by >10%
+
+# find patients with moderate renal impairment: >1 CrCl 30-60 within 48h of
+# enoxaparin start
