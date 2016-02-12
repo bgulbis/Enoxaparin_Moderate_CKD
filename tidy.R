7ad5c9fb9a1ff4cc7b0c61bc2d20fd5bddd43b97
bgulbis <brian.gulbis@gmail.com>
2016-02-12 16:30:55 -0600

    use BGTools to tidy data

--- tidy.R
+++ tidy.R
@@ -6,6 +6,21 @@ source("library.R")
 
 # raw data ----
 
-raw.demograph <- read_edw_data(data.dir, pattern="^demographics")
+raw.blood <- read_edw_data(data.dir, file.name = "blood")
+raw.demograph <- read_edw_data(data.dir, file.name = "demographics")
+raw.diagnosis <- read_edw_data(data.dir, file.name = "diagnosis")
+raw.home.meds <- read_edw_data(data.dir, file.name = "home_meds")
+raw.labs <- read_edw_data(data.dir, file.name = "labs")
+raw.measures <- read_edw_data(data.dir, file.name = "measures")
+raw.meds.cont <- read_edw_data(data.dir, file.name = "meds_continuous")
+raw.meds.sched <- read_edw_data(data.dir, file.name = "meds_sched")
+raw.procedures <- read_edw_data(data.dir, file.name = "procedures")
+raw.radiology <- read_edw_data(data.dir, file.name = "radiology")
+raw.surgeries <- read_edw_data(data.dir, file.name = "surgeries")
 
-tmp <- tidy_edw_data(raw.demograph, "demographics")
+
+# 
+# raw <- list.files(data.dir, pattern = "surgeries", full.names = TRUE) %>%
+#     lapply(read.csv, colClasses = "character") %>%
+#     bind_rows %>%
+#     distinct
