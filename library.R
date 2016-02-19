2599cc735e10742597fbf9f605131936807783a2
bgulbis <brian.gulbis@gmail.com>
2016-02-19 14:19:04 -0600

    work on cont meds

--- library.R
+++ library.R
@@ -11,6 +11,7 @@ library(tidyr)
 # set directories for data
 exclude.dir <- "Exclusion"
 data.dir <- "Data"
+lookup.dir <- "Lookup"
 
 # compress source files
 gzip_files(exclude.dir)
--- tidy.R
+++ tidy.R
@@ -17,7 +17,8 @@ raw.meds.sched <- read_edw_data(data.dir, file.name = "meds_sched")
 raw.procedures <- read_edw_data(data.dir, file.name = "procedures")
 raw.radiology <- read_edw_data(data.dir, file.name = "radiology")
 raw.surgeries <- read_edw_data(data.dir, file.name = "surgeries")
-raw.enox.freq <- read_edw_data(data.dir, file.name = "meds_sched_enox", type = "meds_sched_freq")
+raw.enox.freq <- read_edw_data(data.dir, file.name = "meds_sched_enox", 
+                               type = "meds_sched_freq")
 
 if (!exists("data.patients")) {
     data.patients <- readRDS("included_patients.Rds")
@@ -33,20 +34,36 @@ rm(data.patients)
 
 # diagnosis ----
 # get desired diagnosis codes
-ref.pmh.codes <- read.csv("Lookup/pmh_lookup.csv", colClasses = "character")
+ref.pmh.codes <- read_data(lookup.dir, "pmh_lookup.csv")
 # use standard tidying function
-data.diagnosis <- tidy_data("diagnosis", ref.data = ref.pmh.codes, pt.data = raw.diagnosis, patients = data.demograph) 
+data.diagnosis <- tidy_data("diagnosis", ref.data = ref.pmh.codes, 
+                            pt.data = raw.diagnosis, patients = data.demograph) 
 
 # home meds ----
 # get desired medication classes
-ref.home.meds <- med_lookup(c("anticoagulants", "antiplatelet agents"))
+ref.home.meds <- read_data(lookup.dir, "home_meds_lookup.csv")
 # use standard tidying function
-data.home.meds <- tidy_data("outpt_meds", ref.data = ref.home.meds, pt.data = raw.home.meds, patients = data.demograph)
+data.home.meds <- tidy_data("meds_outpt", ref.data = ref.home.meds, 
+                            pt.data = raw.home.meds, patients = data.demograph)
 
 # hospital meds ----
+
+# get all enoxaparin courses and filter to only included patients
 if (!exists("tmp.enox.courses")) {
     tmp.enox.courses <- readRDS("enoxaparin_courses.Rds")
 }
 
-tmp.enox.courses <- filter(tmp.enox.courses, pie.id %in% incl.pts) %>%
-    select(-last.datetime)
+data.enox.courses <- filter(tmp.enox.courses, pie.id %in% incl.pts) %>%
+    select(-last.datetime, -course.count) %>%
+    mutate(freq = factor(freq, exclude = ""))
+
+# get other medications
+ref.meds.confound <- read_data(lookup.dir, "meds_confound")
+# use standard tidying function
+tmp.meds.cont <- tidy_data("meds_cont", ref.data = ref.meds.confound, 
+                           cont.data = raw.meds.cont, sched.data = raw.meds.sched, 
+                           patients = data.demograph)
+# get running times
+tmp.meds.cont <- calc_runtime(tmp.meds.cont)
+# summarize data
+data.meds.cont <- summarize_cont_meds(tmp.meds.cont)
