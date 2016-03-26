25a2158895d449ac4227b7c3c057492036dd51bd
bgulbis <brian.gulbis@gmail.com>
2016-03-26 11:43:54 -0500

    read and tidy manual data

--- 4-manual.R
+++ 4-manual.R
@@ -5,4 +5,32 @@
 source("0-library.R")
 library(readxl)
 
-raw.manual <- read_excel(paste(manual.dir, "diagnostic_scans.xlsx", sep = "/"))
+raw.manual <- read_excel(paste(manual.dir, "diagnostic_scans.xlsx", sep = "/")) %>%
+    mutate(fin = as.character(fin),
+           thrombus = ifelse(thrombus == 1, TRUE, FALSE),
+           bleed = ifelse(bleed == 1, TRUE, FALSE))
+
+data.manual <- raw.manual %>%
+    inner_join(ref.fins, by = "fin") %>%
+    inner_join(data.enox.courses, by = "pie.id") %>%
+    filter(rad.datetime >= first.datetime,
+           rad.datetime <= end.datetime + days(2),
+           !is.na(thrombus))
+
+data.manual.thrmb <- data.manual %>%
+    filter(thrombus == TRUE) %>%
+    mutate(enox.days = as.numeric(difftime(rad.datetime, first.datetime, units = "days"))) %>%
+    group_by(pie.id) %>%
+    summarize(thrombus = first(thrombus),
+              enox.days = min(enox.days)) %>%
+    filter(enox.days >= 2)
+
+data.manual.bleed <- data.manual %>%
+    filter(bleed == TRUE) %>%
+    mutate(enox.days = as.numeric(difftime(rad.datetime, first.datetime, units = "days")),
+           ct.major = str_detect(rad.type, regex("head|brain", ignore_case = TRUE))) %>%
+    group_by(pie.id) %>%
+    summarize(bleed = first(bleed),
+              enox.days = min(enox.days),
+              ct.major = ifelse(sum(ct.major) > 0, TRUE, FALSE)) %>%
+    filter(enox.days >= 2)
