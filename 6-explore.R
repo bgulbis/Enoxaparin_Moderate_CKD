5a54a34e3c05b6b753f4dbf7034c6ba484abec5f
bgulbis <brian.gulbis@gmail.com>
2016-03-27 16:15:12 -0500

    test exploring

--- 6-explore.R
+++ 6-explore.R
@@ -5,8 +5,15 @@
 age <- analyze.demographics$age
 los <- analyze.demographics$length.stay
 
+age1 <- analyze.demographics[analyze.demographics$group == "moderate", ][["age"]]
+age2 <- analyze.demographics[analyze.demographics$group == "normal", ][["age"]]
+
+qqplot(age1, age2)
 qqnorm(age)
+qqline(age, col = "blue")
 qqnorm(log10(age))
+qqline(log10(age), col = "blue")
+
 qqnorm(los)
 hist(age)
 hist(log10(age))
@@ -18,6 +25,8 @@ n.age <- length(age)
 sd.age <- sd(age)
 (mu.age + c(-1, 1) * qnorm(0.975) * sd.age / sqrt(n.age))
 nrml.age <- (age - mu.age) / sd.age
+qqnorm(nrml.age)
+
 hist(nrml.age)
 library(ggplot2)
 graph <- ggplot(analyze.demographics, aes(x = age)) +
